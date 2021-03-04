6fc531f1df8b7dc2bb6fcf95b41b3c58e3b36f13
Yağmur Durukan <ygmrdrkn99@gmail.com>
2021-03-05 00:47:33 +0300

    end of the 10th day

---
+++ ReCapProject/Business/Concrete/CustomerManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public IResult Add(Customer customer)
+        {
+            _customerDal.Add(customer);
+            return new SuccessResult(Messages.CustomerAdded);
+        }
+
+        public IResult Delete(Customer customer)
+        {
+            _customerDal.Delete(customer);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            if (DateTime.Now.Hour == 16)
+            {
+                return new ErrorDataResult<List<Customer>>(Messages.SystemBuilding);
+            }
+            return new SuccessDataResult<List<Customer>>();
+        }
+
+        public IDataResult<Customer> GetById(int id)
+        {
+            return new SuccessDataResult<Customer>(_customerDal.Get(c=> c.Id == id));
+        }
+
+        public IResult Update(Customer customer)
+        {
+            _customerDal.Update(customer);
+            return new SuccessResult();
+        }
+    }
+}
--- ReCapProject/Business/Constants/Messages.cs
+++ ReCapProject/Business/Constants/Messages.cs
@@ -13,6 +13,12 @@ namespace Business.Constants
         public static string BrandAdded = "Brand is added";
         public static string ColorDeleted = "Color is deleted";
         public static string ColorUpdated = "Color is updated";
+        public static string UserAdded = "User is added";
+        public static string UserDeleted = "User is deleted";
+        public static string CustomerAdded = "Customer is added";
+        public static string SystemBuilding = "System is building";
+        public static string NotReturned = "The car is not returned yet";
+        public static string SuccessfulRent = "Renting is successful";
 
     }
 }
