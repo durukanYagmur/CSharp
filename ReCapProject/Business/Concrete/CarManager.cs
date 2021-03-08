955a3e2db0c858920f3289dac3f3428b0b5c2024
Yağmur Durukan <ygmrdrkn99@gmail.com>
2021-03-08 22:58:11 +0300

    validation is added

--- ReCapProject/Business/Concrete/CarManager.cs
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -23,17 +25,13 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Car>>(_carDal.GetAll()); 
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.Description.Length>2 && car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
-            }
-            else
-            {
-                return new ErrorResult(Messages.InvalidEntry);
-            }
+            
+             _carDal.Add(car);
+             return new SuccessResult(Messages.CarAdded);
+            
         }
 
         public IResult Delete(Car car)
---
+++ ReCapProject/Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator: AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Description).MinimumLength(2).WithMessage("Car name must contain at least 2 char");
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+        }
+    }
+}
